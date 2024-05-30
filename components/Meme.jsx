3aa3e829245fb9252bb6f0fec583ba7681fe98a4
Franz <franzj.pretorius.2003@gmail.com>
2024-05-30 22:59:19 +0200

    Created meme section

---
+++ components/Meme.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+
+//Form section of the app
+export default function Meme() {
+    return (
+        <main>
+            <form className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button 
+                    className="form--button"
+                >
+                    Get a new meme image 🖼
+                </button>
+            </form>
+        </main>
+    )
+}
