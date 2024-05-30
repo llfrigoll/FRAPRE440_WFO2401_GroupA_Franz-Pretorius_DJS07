e7a41e82b1a833cdff9ebc27da5c6366d4fb03ee
Franz <franzj.pretorius.2003@gmail.com>
2024-05-30 23:27:45 +0200

    Added array of memes and added functionality to display random meme to the UI

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -1,10 +1,23 @@
-import React from "react"
+import React from 'react'
+import memesData from '../memesData.jsx'
 
 //Form section of the app
 export default function Meme() {
+
+    //Variable holding the destructured current state and its dispatch function
+    const [memeImage, setMemeImage] = React.useState("https://i.imgflip.com/1g8my4.jpg")
+
+    //Function that grabs all the memes from the array, generates a random number and uses that number to select a random meme from the array,
+    //thereafter it sets the current state to that random meme
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+    }
+
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input 
                     type="text"
                     placeholder="Top text"
@@ -17,10 +30,12 @@ export default function Meme() {
                 />
                 <button 
                     className="form--button"
+                    onClick={getMemeImage}
                 >
                     Get a new meme image 🖼
                 </button>
-            </form>
+            </div>
+            <img src={memeImage} className='meme--image'/>
         </main>
     )
 }
